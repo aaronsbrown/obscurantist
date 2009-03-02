1d3a17317bcdad2fa2704a05351be91149cc8b56
Aaron Brown <aaron.s.brown+git@gmail.com>
2009-03-01 23:49:07 -0800

    initial commit

--- javascript/application.js
+++ javascript/application.js
@@ -1,2 +1,6 @@
 $(function() {
-});
+        $('#grid-switch a').click(function() {
+                $('#container').toggleClass("grid-overlay");
+                $('body').toggleClass("baseline-grid-overlay");
+            });
+    });
