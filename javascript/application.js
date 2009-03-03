da9c0c208bf19282b1a0946b4d901eca4d1d4ddd
Aaron Brown <aaron.s.brown@gmail.com>
2009-03-02 22:38:05 -0800

    renamed footer to tagline; removed baseline grid background; increased caption size

--- javascript/application.js
+++ javascript/application.js
@@ -1,6 +1,6 @@
 $(function() {
         $('#grid-switch a').click(function() {
                 $('#container').toggleClass("grid-overlay");
-                $('body').toggleClass("baseline-grid-overlay");
+                //                $('body').toggleClass("baseline-grid-overlay");
             });
     });
